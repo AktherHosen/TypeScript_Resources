0b10fa7beab3bef08e6c45ad068bbf15755503c6
Md Akther Hosen <dev.aktherhosen@gmail.com>
2025-05-01 02:18:46 +0600

    first module resources

---
+++ module1/dist/1.4.js
@@ -0,0 +1,20 @@
+"use strict";
+// Basic data type
+var firstName = "aktherhosen";
+var roolNumber = 123;
+var isAdmin = true;
+var x = undefined;
+var y = null;
+// any type
+var d;
+d = 123;
+d = "strings";
+d = true;
+var e;
+e = 123;
+var friends = ["rachel", "monicha"];
+var eligibleRollList = [1, 2];
+eligibleRollList.push(1);
+// tuple --> array --> order --> type of value
+var coordinates = [1, 2];
+var ageName = [25, "akther"];
